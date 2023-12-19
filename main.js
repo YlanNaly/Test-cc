d5b0ee268fb2dcb012a8a01846b1fbc9279a8b8f
Ylan <hei.nalisoa@gmail.com>
2023-12-19 09:34:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+import promptSync from 'prompt-sync';
+import {displayMenu} from "./menu.js";
+const prompt = promptSync();
+
+export const riceCooker = {
+  ricePresent: false,
+  riceCooked: false,
+  steamingInProgress: false,
+  heatingInProgress: false,
+
+  addRice() {
+    if (!this.ricePresent) {
+      this.ricePresent = true;
+      console.log('Rice has been added.');
+    } else {
+      console.log('There\'s already rice in the rice cooker.');
+    }
+  },
+
+  cookRice() {
+    if (this.ricePresent && !this.riceCooked) {
+      console.log('Cooking rice...');
+      this.delaySync(1500);
+      this.riceCooked = true;
+      console.log('The rice has been cooked!');
+    } else if (!this.ricePresent) {
+      console.log('Cannot cook. The rice cooker is empty.');
+    } else {
+      console.log('The rice is already cooked.');
+    }
+  },
+
+  steam() {
+    if (this.ricePresent && !this.steamingInProgress) {
+      console.log('Steaming in progress...');
+      this.steamingInProgress = true;
+      this.delaySync(1500);
+      this.steamingInProgress = false;
+      console.log('Steaming completed!');
+    } else if (!this.ricePresent) {
+      console.log('Cannot steam. The rice cooker is empty.');
+    } else {
+      console.log('Steaming is already in progress.');
+    }
+  },
+
+  removeRice() {
+    if (this.ricePresent && (this.riceCooked || this.heatingInProgress)) {
+      this.ricePresent = false;
+      this.riceCooked = false;
+      this.steamingInProgress = false;
+      this.heatingInProgress = false;
+      console.log('The rice has been removed from the rice cooker.');
+    } else {
+      console.log('There\'s no rice to remove or it is not cooked yet.');
+    }
+  },
+
 
 keepWarm(){
 	if(this.ricePresent){
@@ -32,7 +90,9 @@ export function simulateRiceCooker(){
 				case 2 : 
 					riceCooker.cookRice();
 					break;
-				...
+				/**
+    				The rest of case
+    				*/
 				break;
 				default:
 					console.log("invalid input");
@@ -40,3 +100,4 @@ export function simulateRiceCooker(){
 		}
 	}
 }
+simulateRiceCooker();
